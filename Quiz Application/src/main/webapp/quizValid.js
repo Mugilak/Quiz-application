27d00055593120c87e0e3e038904e3788e752c40
Mugila kanagaraj <81410764+Mugilak@users.noreply.github.com>
2023-05-10 21:36:19 +0530

    commit

--- Quiz Application/src/main/webapp/quizValid.js
+++ Quiz Application/src/main/webapp/quizValid.js
@@ -22,59 +22,7 @@ const Questions = [
 		correct: "op1"
 	}
 ];
-/*
-var array = function print() {
-	var count = Object.keys(myObject).length;
-	for (var id = 1; id <= count; id++) {
-		answer = [];
-		const num = document.getElementsByClassName("num");
-		num.innerText = id;
 
-		const ques = document.getElementsByClassName("question");
-		ques.innerText = questions[id].q;
-
-		const op1 = document.getElementsByClassName("op1");
-		const op2 = document.getElementsByClassName("op2");
-		const op3 = document.getElementsByClassName("op3");
-		const op4 = document.getElementsByClassName("op4");
-		op1.innerText = questions[id].ans[0].op1;
-		op2.innertext = questions[id].ans[1].op2;
-		op3.innerText = questions[id].ans[2].op3;
-		op4.innerText = questions[id].ans[3].op4;
-		
-		  var selected = "";
-  
-	// Show selection for op1
-	op1.addEventListener("click", () => {
-		op1.style.backgroundColor = "lightgoldenrodyellow";
-		selected = op1.value;
-	})
-  
-	// Show selection for op2
-	op2.addEventListener("click", () => {
-		op2.style.backgroundColor = "lightgoldenrodyellow";
-		selected = op2.value;
-	})
-  
-	// Show selection for op3
-	op3.addEventListener("click", () => {
-		op3.style.backgroundColor = "lightgoldenrodyellow";
-		selected = op3.value;
-	})
-  
-	// Show selection for op4
-	op4.addEventListener("click", () => {
-		op4.style.backgroundColor = "lightgoldenrodyellow";
-		selected = op4.value;
-	}) 
-    
-	};
-	var start=true;
-	if(start){
-		print();
-	}
-}
-*/
 function start() {
 	var quizContainer = document.getElementById('question');
 	var resultsContainer = document.getElementById('result');
@@ -104,8 +52,8 @@ function generateQuiz(questions, quizContainer, resultsContainer, submitButton) 
 			}
 
 			output.push(
-				'<div class="question">' + questions[i].num +' . '+ questions[i].q + '</div><br>'
-				+ '<div class="answers">' + answers.join('') + '</div><br><br>'
+				'<div class="question">' + questions[i].num + ' . ' + questions[i].q + '</div><br>'
+				+ '<div class="answers">' + answers.join('') + '</div><br><div class="required"></div><br>'
 			);
 		}
 
@@ -114,43 +62,38 @@ function generateQuiz(questions, quizContainer, resultsContainer, submitButton) 
 
 
 	function showResults(questions, quizContainer, resultsContainer) {
-
-		// gather answer containers from our quiz
+		var unAnswered = false;
 		var answerContainers = quizContainer.querySelectorAll('.answers');
+		var statement = quizContainer.querySelectorAll('.required');
 
-		// keep track of user's answers
 		var userAnswer = '';
 		var numCorrect = 0;
 
-		// for each question...
 		for (var i = 0; i < questions.length; i++) {
 
-			// find selected answer
 			userAnswer = (answerContainers[i].querySelector('input[name=question' + i + ']:checked') || {}).value;
-
-			// if answer is correct
+			
 			if (userAnswer === questions[i].correct) {
-				// add to the number of correct answers
 				numCorrect++;
-
-				// color the answers green
 				answerContainers[i].style.color = 'green';
+			} else if (userAnswer === undefined) {
+				unAnswered=true;
+                statement[i].innerHTML = '*answer not chosen'+'<br>';
+                statement[i].style.color = 'red';
 			}
-			// if answer is wrong or blank
 			else {
-				// color the answers red
 				answerContainers[i].style.color = 'red';
 			}
 		}
-
-		// show number of correct answers out of total
-		resultsContainer.innerHTML = 'SCORE : '+numCorrect + ' out of ' + questions.length;
+		if(!unAnswered){
+		resultsContainer.innerHTML = 'SCORE : ' + numCorrect + ' out of ' + questions.length;
+		}else{
+			resultsContainer.innerHTML='Some questions are not answered.. Please check before submit'
+		}
 	}
 
-	// show questions right away
 	showQuestions(questions, quizContainer);
 
-	// on submit, show results
 	submitButton.onclick = function() {
 		showResults(questions, quizContainer, resultsContainer);
 	}
